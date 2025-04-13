943df9772c19158b511072f1e1c2d4428a69eeca
tablewebsite <talkingtothemoon189@gmail.com>
2025-04-13 15:27:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,162 @@
+function pt1() {
+    alert("can you tell i'm trying to format this into a letter");
+}
+
+function pt2() {
+    alert("i still find this insane to believe. only three years?? i meant it when i said it felt like five, or maybe even that i've known you my entire life");
+}
+
+function pt3() {
+    alert("it probably is")
+}
+
+function pt4() {
+    alert("empathsis on really, like really, really, really")
+}
+
+function pt5() {
+    alert("probably dead or something LMFAOOO")
+}
+
+function pt6() {
+    alert("i remember you said a while back that when i text you a lot, you feel like it's a lot to respond to")
+    alert("i've been trying to text you less causea that, please dont feel bad")
+}
+
+function pt7() {
+    alert("i say this often, i think i've said it the past two letters (?) i've written you")
+    alert("it still holds true, despite it - i hope you always remember this, if not, i'm here to remind you")
+}
+
+function pt8() {
+    alert("i still remember your graduation, even though it was only what, seven months ago? i remember thinking, 'wow, she did it!!! i knew she could!!! im so happy!'")
+    alert("even though the day might've been ruined, you did it, brin, that's what matters in the end")
+}
+
+function pt9() {
+    alert("this is three years ago, i don't know why i'm acting like it's been three decades")
+}
+
+function pt10() {
+    alert("please don't think i'm complaining that you don't text as often, i really don't mind")
+    alert("i'll only mind if i find out you aren't okay. take that as a threat")
+}
+
+function pt11() {
+    alert("this might be a little weird to still do - i apologize")
+}
+
+function pt12() {
+    alert("sentihua is probably still the best ship in hi3, if you ask me")
+}
+
+function pt13() {
+    alert("you won't be rid of me easily, i promise you this")
+}
+
+function pt14() {
+    alert("i could be asleep for all i care, just send a text, call even - you have my number after all LMFAO")
+}
+
+function pt15() {
+    alert("n i mean always")
+}
+
+function pt16() {
+    alert("i take my words very seriously, at least when it comes to things like this")
+}
+
+function pt17() {
+    alert("i've been writing on twine, maybe that's why i had the whole click and read extra text idea")
+}
+
+function pt18() {
+    alert("at least from what i know")
+}
+
+function pt19() {
+    alert("being poor is so annoying")
+}
+
+function pt20() {
+    alert("as long as you don't mind/want the title, anyways")
+}
+
+function pt21() {
+    alert("again, unless you don't want it to change")
+}
+
+function pt22() {
+    alert("even if those things haven't involved me")
+    alert("i'm losing the plot, just maybe")
+}
+
+function pt23() {
+    alert("this was on purpose i swear, i'm bringing back the little inside joke")
+}
+
+function pt24(){
+    alert("you probably don't feel the same way - i'd hope not, anyways")
+}
+
+function pt25(){
+    alert("i think i send sunflower pictures a lot though, all things considered")
+}
+
+function pt26(){
+    alert("swear i've writen about this somehow")
+}
+
+function pt27(){
+    alert("i'm writing this the day after")
+}
+
+function pt28(){
+    alert("i'm writing paragraphs as the days get closer to our anniversary - if i didn't already say")
+}
+
+function pt29(){
+    alert("when i'm writing this, anyways")
+}
+
+function pt30(){
+    alert("'am', rather LMFAO")
+}
+
+function pt31(){
+    alert("it's kinda what i was aiming for, but yknow, i wanna apologize regardless")
+}
+
+function pt32(){
+    alert("i hope so anyways")
+}
+
+function pt33(){
+    alert("i'm poor n were like hours apart")
+    alert("time-wise n irl-wirse; if i can put it that way anyways")
+}
+
+function pt34(){
+    alert("i hope i got you eventually though; i really wanna make sure this reads like a letter but lets you listen to your favourite music too")
+    alert("i can't just play niigo n ave mujica LMFAOO")
+}
+
+function pt35(){
+    alert("you did, though i'm not sure if lucky or daylight would've worked for something like this :{")
+}
+
+function pt36(){
+    alert("i don't know why i did, it just happened LMFAO")
+}
+
+function pt37(){
+    alert("i feel like you might've guessed this already")
+}
+
+function pt38(){
+    alert("n watching dog movies/movies in general was my tactic, so to speak")
+}
+
+function pt39(){
+    alert("it feels fresh for me, considering i'm writing this on the day LMFAO")
+}
